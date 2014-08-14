c350f3c363f19e2b6722a76aa0e3cd2dd48c4038
vence <venceslas.prouteau@hotmail.fr>
2014-08-14 16:56:01 +0200

    feat(js): add ui-router instead of ngRoute

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,27 +9,30 @@
  * Main module of the application.
  */
 angular
-  .module('lolApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/champions', {
-        templateUrl: 'views/champions.html',
-        controller: 'ChampionsCtrl'
-      })
-      .when('/summoners', {
-        templateUrl: 'views/summoners.html',
-        controller: 'SummonersCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+    .module('lolApp', [
+        'ngCookies',
+        'ngResource',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        //
+        // For any unmatched url, redirect to /state1
+        $urlRouterProvider.otherwise("/index.html");
+        //
+        // Now set up the states
+    $stateProvider
+        .state('champions', {
+            url: "/champions",
+            templateUrl: "views/champions.html",
+            controller: "ChampionsCtrl"
+        })
+        .state('summoners', {
+            url: "/summoners",
+            templateUrl: "views/summoners.html",
+            controller: "SummonersCtrl"
+        })
   })
   .run(function(ChampionService){
     ChampionService.getChampion();
